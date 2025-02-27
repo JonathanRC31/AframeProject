300a9a0aedf74b17ffa8b03417370cf1f51b86d8
JonathanRC31 <jonathanc474@nycstudents.net>
2025-02-27 14:45:10 -0500

    Update ladder.js

--- MyProject/ladder.js
+++ MyProject/ladder.js
@@ -2,25 +2,25 @@ class Ladder{
   constructor(x,y,z){
     this.obj = document.createElement("a-entity")
 
-    for(let i = 0; i < 3; i++){
-      let walls = document.createElement("a-cylinder");
-      walls.setAttribute("position",{x:0,y:0 + 1 + (i * 0.5),z:0});
-      walls.setAttribute("rotation",{x:0,y:0,z:90});
-      walls.setAttribute("radius",0.15);
-      walls.setAttribute("color","blue")
-      walls.setAttribute("height",2);
-      walls.setAttribute("static-body","");
-      this.obj.append(walls)
+    for(let i = 0; i < 6; i++){
+      let steps = document.createElement("a-cylinder");
+      steps.setAttribute("position",{x:0,y:0 + 1 + (i * 0.5),z:0});
+      steps.setAttribute("rotation",{x:0,y:0,z:90});
+      steps.setAttribute("radius",0.75);
+      steps.setAttribute("color","blue")
+      steps.setAttribute("height",2);
+      steps.setAttribute("static-body","");
+      this.obj.append(steps)
     }
 	
     for(let i = 0; i < 2; i++){
-      let beam = document.createElement("a-cylinder");
-      beam.setAttribute("position",{x:2 + (-i * 2),y:2,z:2});
-      beam.setAttribute("radius",0.15);
-      beam.setAttribute("color","gray")
-      beam.setAttribute("height",4);
-      beam.setAttribute("static-body","");
-      this.obj.append(beam)
+      let handles = document.createElement("a-cylinder");
+      handles.setAttribute("position",{x:1 + (-i * 2),y:2,z:2});
+      handles.setAttribute("radius",0.15);
+      handles.setAttribute("color","gray")
+      handles.setAttribute("height",4);
+      handles.setAttribute("static-body","");
+      this.obj.append(handles)
     }
 
     this.obj.setAttribute("position",{x:x,y:y,z:z});
