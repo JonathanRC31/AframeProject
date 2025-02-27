11b748ba9c0bbd23d87a557247951e4b257b6601
JonathanRC31 <jonathanc474@nycstudents.net>
2025-02-27 14:23:04 -0500

    Update ladder.js

--- MyProject/ladder.js
+++ MyProject/ladder.js
@@ -8,12 +8,20 @@ class Ladder{
       walls.setAttribute("rotation",{x:0,y:0,z:90});
       walls.setAttribute("radius",0.15);
       walls.setAttribute("color","blue")
-      walls.setAttribute("height",5);
-	    walls.setAttribute("static-body","");
+      walls.setAttribute("height",2);
+      walls.setAttribute("static-body","");
       this.obj.append(walls)
     }
 	
-
+    for(let i = 0; i < 2; i++){
+      let beam = document.createElement("a-cylinder");
+      beam.setAttribute("position",{x:2 + (-i * 2),y:2,z:2});
+      beam.setAttribute("radius",0.15);
+      beam.setAttribute("color","gray")
+      beam.setAttribute("height",4);
+      beam.setAttribute("static-body","");
+      this.obj.append(beam)
+    }
 
     this.obj.setAttribute("position",{x:x,y:y,z:z});
     scene.append(this.obj);
